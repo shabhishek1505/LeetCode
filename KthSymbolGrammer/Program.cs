ff5fe01c98865843a3e5812f0aadb22d8397813c
Abhishek Sharma <abhishek.sharma@hach.com>
2020-07-11 10:28:36 +0530

    Kth Grammar

---
+++ KthSymbolGrammer/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KthSymbolGrammar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var output = GetKthGrammar(4, 0);
+        }
+
+
+        public static int GetKthGrammar(int n, int k)
+        {
+            if (n == 0)
+                return 0;
+
+            var temp = GetKthGrammar(n - 1, k / 2);
+            if (k % 2 == 0)
+                return temp;
+
+            return temp == 0 ? 1 : 0;
+
+        }
+
+    }
+}
