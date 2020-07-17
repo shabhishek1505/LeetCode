639bd8c833a5d3dc4e6af3dd4e00cbc11191511a
Abhishek Sharma <abhishek.sharma@hach.com>
2020-07-17 10:36:15 +0530

    17/07

---
+++ PermutationWithSpaces/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PermutationWithSpaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintPermutation("A","BCDEF");
+        }
+
+        public static void PrintPermutation(string output,string input)
+        {
+            
+            if (input.Length == 0)
+            {
+                Console.WriteLine(output+input);
+                return;
+            }
+
+            var newInp = input.Substring(1);
+
+            PrintPermutation(output+"_"+input.ToCharArray().First(),newInp);
+
+            PrintPermutation(output + input.ToCharArray().First(), newInp);
+
+        }
+    }
+}
