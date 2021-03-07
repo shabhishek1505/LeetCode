2df5ffafcaa5e6875dd510337265ec7990412031
Abhishek Sharma <abhishek.sharma1592@yahoo.com>
2021-03-07 12:37:54 +0530

    Leet Code array

---
+++ RotateArray/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace RotateArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inp = new int[] { 1, 2, 3, 4 };
+            Rotate1(inp, 2);
+        }
+
+        public static void Rotate1(int[] nums, int k)
+        {
+           
+            k = k % nums.Length;
+            int count = 0;
+            for (int start = 0; count < nums.Length; start++)
+            {
+                int pointer = start;
+                int temp = nums[pointer];
+                do
+                {
+                    int i = (pointer + k) % (nums.Length);
+                    int temp1 = nums[i];
+                    nums[i] = temp;
+                    temp = temp1;
+                    pointer = i;
+                    count++;
+                    
+                } while (start != pointer);
+            }
+            
+        }
+
+        public static void Rotate(int[] nums, int k)
+        {
+
+            int i = nums.Length - k - 1;
+            int temp = 0;
+
+            temp = nums[i];
+            while (i > 0)
+            {
+                int temp1 = nums[i + k];
+                nums[i + k] = temp;
+                temp = nums[nums.Length - i - 2];
+                nums[nums.Length - i - 2] = temp1;
+                i--;
+            }
+        }
+
+
+    }
+}
---
+++ StocksBuySell/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace StocksBuySell
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var prices = new int[] { 7, 1, 5, 3, 6, 4 };
+            var op = calculate(prices, 0);
+            var o = maxprofit(prices);
+            var p = maxprofit(prices);
+        }
+
+        static int calculate(int[] prices, int s)
+        {
+            if (s >= prices.Length)
+                return 0;
+            int max = 0;
+            for (int start = s; start < prices.Length; start++)
+            {
+                int maxProfit = 0;
+                for (int i = start + 1; i < prices.Length; i++)
+                {
+                    if (prices[start] < prices[i])
+                    {
+                        int profit = prices[i] - prices[start] + calculate(prices, i + 1);
+
+                        maxProfit = Math.Max(maxProfit, profit);
+                    }
+                }
+                max = Math.Max(max, maxProfit);
+            }
+            return max;
+        }
+
+        static int maxprofit(int[] prices)
+        {
+            int i = 0;
+            int maxprofit = 0;
+            int valley = prices[0];
+            int peak = prices[0];
+
+            while (i < prices.Length - 1)
+            {
+                while (i < prices.Length - 1 && prices[i + 1] <= prices[i])
+                {
+                    i++;
+                }
+                valley = prices[i];
+                while (i < prices.Length - 1 && prices[i + 1] >= prices[i])
+                {
+                    i++;
+                }
+                peak = prices[i];
+                maxprofit += peak - valley;
+            }
+            return maxprofit;
+        }
+
+        static int maxprofit1(int[] prices)
+        {
+            int maxprofit = 0;
+
+            for (int i = 1; i < prices.Length; i++)
+            {
+                if (prices[i] > prices[i - 1])
+                {
+                    maxprofit += prices[i] - prices[i - 1];
+                }
+            }
+
+            return maxprofit;
+        }
+    }
+}
